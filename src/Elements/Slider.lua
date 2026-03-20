2d3f734206026da1a09dd2aa967394161d9eac96
Kqmeta <94103858+kqmeta@users.noreply.github.com>
2026-03-20 02:38:18 +0200

    Update Slider.lua

--- src/Elements/Slider.lua
+++ src/Elements/Slider.lua
@@ -38,7 +38,7 @@ function Element:New(Idx, Config)
 		AnchorPoint = Vector2.new(0, 0.5),
 		Position = UDim2.new(0, -7, 0.5, 0),
 		Size = UDim2.fromOffset(14, 14),
-		Image = "http://www.roblox.com/asset/?id=12266946128",
+		Image = "",
 		ThemeTag = {
 			ImageColor3 = "Accent",
 		},
